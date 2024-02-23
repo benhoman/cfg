1ba4176c9a7be67d06c0d6597794169eb07e8b36
Ben Homan <ben@benhoman.com>
2024-02-22 22:19:48 -0500

    initial waybar

---
+++ .config/waybar/scripts/scratchpads.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import json
+import subprocess
+import sys
+
+def findScratchpads(sway_tree: str) -> int:
+    # sway_tree = subprocess.run(
+    #     ["swaymsg", "-t", "get_tree"],
+    #     stdout=subprocess.PIPE
+    # ).stdout
+
+    data = json.loads(sway_tree)
+
+    scratchpad_count = len(data["nodes"][0]["nodes"][0]["floating_nodes"])
+
+    return scratchpad_count
+
+
+print(findScratchpads(sys.stdin.read()))
