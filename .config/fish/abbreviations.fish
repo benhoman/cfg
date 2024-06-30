6df74679a4f3388da89435ba289475c8de82dff8
Ben Homan <ben@benhoman.com>
2024-06-29 22:34:31 -0400

    update abbr

--- .config/fish/abbreviations.fish
+++ .config/fish/abbreviations.fish
@@ -1,19 +1,19 @@
 if type -q eza
-  abbr --add l "eza -l --icons --git -a --color=auto"
-  abbr --add ls "eza --color=auto"
-  abbr --add ll "eza --color=auto --icons -lah"
-  abbr --add lg "eza --color=auto --icons --grid -lah"
-  abbr --add la "eza --color=auto --icons -a"
+    abbr --add l "eza -l --icons --git -a --color=auto"
+    abbr --add ls "eza --color=auto"
+    abbr --add ll "eza --color=auto --icons -lah"
+    abbr --add lg "eza --color=auto --icons --grid -lah"
+    abbr --add la "eza --color=auto --icons -a"
 else
-  abbr --add ls "ls --color"
+    abbr --add ls "ls --color"
 end
 
 abbr --add lgrep "ls -lha | grep"
 
 if type -q batcat
-  abbr --add cat "batcat"
+    abbr --add cat batcat
 else if type -q bat
-  abbr --add cat "bat"
+    abbr --add cat bat
 end
 
 # Docker
@@ -28,14 +28,13 @@ abbr --add bdc 'black --diff --color'
 abbr --add pip-dep 'pip --use-deprecated=legacy-resolver'
 
 # Git
-abbr --add g 'git'
+abbr --add g git
 abbr --add ga 'git add'
 abbr --add gc 'git checkout'
 abbr --add gm 'git commit'
 abbr --add gs 'git status'
 abbr --add gpuq 'git pull upstream qa'
+abbr --add gpus 'git pull upstream staging'
 abbr --add gcob 'git branch | fzf | xargs git checkout'
 # abbr --add gdnew "for next in \$( git ls-files --others --exclude-standard ) ; do git --no-pager diff --no-index /dev/null \$next; done;"
 abbr --add gdall "git --no-pager diff; gdnew"
-
-
