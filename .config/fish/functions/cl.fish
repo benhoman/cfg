da8e8c3d85c46eb9dba206e0805f4a0ff523dc15
Ben Homan <ben@benhoman.com>
2024-09-05 14:20:45 -0400

    update copy last function

--- .config/fish/functions/cl.fish
+++ .config/fish/functions/cl.fish
@@ -1,4 +1,4 @@
 function cl -d 'copy output of last command to clipboard'
     set -l last (history --max=1|sed -e 's/^ +//')
-    eval $last | fish_clipboard_copy
+    eval $last | string collect | fish_clipboard_copy
 end
